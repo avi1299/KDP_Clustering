0c1704d38c9a1336bd058cc3c08da4000a11ceeb
Avinash <avi1299@gmail.com>
2021-05-27 16:27:41 +0530

    Added verbosity control Modified the input module and introduced PDB_reader_universal for handling differently ordered input. Modified the makefile Added more pdb for analysis

--- Week 1/My_code/src/input.c
+++ Week 1/My_code/src/input.c
@@ -1,5 +1,7 @@
 #include "input.h"
 
+
+//Assumptions: All the molecules are listed together and in order of molecules P OHL HOL OHL HOL O2L O2L
 void PDB_reader(FILE* fp_in,HPO molecules[],coordinates boxlength,int *no_of_molecules,int *start_mol_no)
 {
     int i,atom_no,mol_no;
@@ -12,7 +14,7 @@ void PDB_reader(FILE* fp_in,HPO molecules[],coordinates boxlength,int *no_of_mol
     }
     sscanf(line,"%*s %lf %lf %lf %*lf %*lf %*lf %*s %*d %*d",&boxlength[0],&boxlength[1],&boxlength[2]);
     //printf("%s",line);
-    printf("BoxLength = %lf %lf %lf \n",boxlength[0],boxlength[1],boxlength[2]);
+    //printf("BoxLength = %lf %lf %lf \n",boxlength[0],boxlength[1],boxlength[2]);
     for(;;)
     {
         if(fgets(line, LLEN, fp_in) == NULL)
@@ -79,6 +81,177 @@ void PDB_reader(FILE* fp_in,HPO molecules[],coordinates boxlength,int *no_of_mol
                 //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
                 for(i=0;i<3;i++)
                     molecules[mol_no-*start_mol_no].O2L_2[i]=coordinate[i];
+            }
+        
+        }
+    }
+}
+
+//Assumptions: All the molecules are listed together and in order of molecules OHL HOL P O2L OHL HOL O2L
+void PDB_reader_alternate(FILE* fp_in,HPO molecules[],coordinates boxlength,int *no_of_molecules,int *start_mol_no)
+{
+    int i,atom_no,mol_no;
+    char line[LLEN],atom_name[LLEN],mol_name[LLEN];
+    coordinates coordinate;
+    if(fgets(line, LLEN, fp_in) == NULL)
+    {
+        printf("Error: infile is empty!\n"); 
+        exit(1);
+    }
+    sscanf(line,"%*s %lf %lf %lf %*lf %*lf %*lf %*s %*d %*d",&boxlength[0],&boxlength[1],&boxlength[2]);
+    //printf("%s",line);
+    //printf("BoxLength = %lf %lf %lf \n",boxlength[0],boxlength[1],boxlength[2]);
+    for(;;)
+    {
+        if(fgets(line, LLEN, fp_in) == NULL)
+            break;
+        sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+        //Executes below when it encounters HPO molecules. It extracts information for every atom in every HPO molecule 
+        if(strcmp(mol_name,"HPO")==0)
+        {
+            //Increasing the count of HPO molecules
+            (*no_of_molecules)++;
+            //Noting where the first HPO molecule was encountered
+            if(*start_mol_no==-1)
+                *start_mol_no=mol_no;
+            //For the OHL_1 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].OHL_1[i]=coordinate[i];
+
+            }
+            //For the HOL_1 atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].HOL_1[i]=coordinate[i];
+            }            
+            //For the P atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].P[i]=coordinate[i];
+            }
+            //For the O2L_1 atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].O2L_1[i]=coordinate[i];
+            }
+            //For the OHL_2 atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].OHL_2[i]=coordinate[i];
+            }
+            //For the HOL_2 atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].HOL_2[i]=coordinate[i];
+            }
+            //For the O2L_2 atom
+            {
+                fgets(line, LLEN, fp_in);
+                sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].O2L_2[i]=coordinate[i];
+            }
+        
+        }
+    }
+}
+
+//Assumption all the molecules are listed together and though order of molecules doesn't matter, all the 7 atoms corresponding to a molecule should be together
+//i.e. one after the other.
+void PDB_reader_universal(FILE* fp_in,HPO molecules[],coordinates boxlength,int *no_of_molecules,int *start_mol_no)
+{
+    int i,atom_no,mol_no;
+    char line[LLEN],atom_name[LLEN],mol_name[LLEN];
+    int HOL_read=0;
+    int OHL_read=0;
+    int O2L_read=0;
+    coordinates coordinate;
+    if(fgets(line, LLEN, fp_in) == NULL)
+    {
+        printf("Error: infile is empty!\n"); 
+        exit(1);
+    }
+    sscanf(line,"%*s %lf %lf %lf %*lf %*lf %*lf %*s %*d %*d",&boxlength[0],&boxlength[1],&boxlength[2]);
+    //printf("%s",line);
+    //printf("BoxLength = %lf %lf %lf \n",boxlength[0],boxlength[1],boxlength[2]);
+    for(;;)
+    {
+        if(fgets(line, LLEN, fp_in) == NULL)
+            break;
+        sscanf(line,"%*s %d %s %s X %d %lf %lf %lf %*lf %*lf",&atom_no,atom_name,mol_name,&mol_no,&coordinate[0],&coordinate[1],&coordinate[2]);
+        //Executes below when it encounters HPO molecules. It extracts information for every atom in every HPO molecule 
+        if(strcmp(mol_name,"HPO")==0)
+        {
+            //Noting where the first HPO molecule was encountered
+            if(*start_mol_no==-1)
+                *start_mol_no=mol_no;
+            if (strcmp(atom_name,"PL")==0)             //For the P atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].P[i]=coordinate[i];
+                //Increasing the count of HPO molecules
+                (*no_of_molecules)++;
+            }
+            else if (!(OHL_read)&&(strcmp(atom_name,"OHL")==0))//For the OHL_1 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].OHL_1[i]=coordinate[i];
+                OHL_read=1;
+            }
+            else if (!(HOL_read)&&(strcmp(atom_name,"HOL")==0))//For the HOL_1 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].HOL_1[i]=coordinate[i];
+                HOL_read=1;
+            }
+            else if ((OHL_read)&&(strcmp(atom_name,"OHL")==0))//For the OHL_2 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].OHL_2[i]=coordinate[i];
+                OHL_read=0;
+            }
+            else if ((HOL_read)&&(strcmp(atom_name,"HOL")==0))//For the HOL_2 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].HOL_2[i]=coordinate[i];
+                HOL_read=0;
+            }
+            else if (!(O2L_read)&&(strcmp(atom_name,"O2L")==0))//For the O2L_1 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].O2L_1[i]=coordinate[i];
+                O2L_read=1;
+            }
+            else if ((O2L_read)&&(strcmp(atom_name,"O2L")==0))//For the O2L_2 atom
+            {
+                //printf("%d %s %s %d %lf %lf %lf\n",atom_no,atom_name,mol_name,mol_no,coordinate[0],coordinate[1],coordinate[2]);
+                for(i=0;i<3;i++)
+                    molecules[mol_no-*start_mol_no].O2L_2[i]=coordinate[i];
+                O2L_read=0;
             }
         
         }
--- Week 1/My_code/src/test.c
+++ Week 1/My_code/src/test.c
@@ -25,6 +25,27 @@ void adjacency_list_constructor(HPO molecules[],coordinates boxlength,int no_of_
             {
                 add_node_given_value(&adjacency_list[i],j);
                 add_node_given_value(&adjacency_list[j],i);
+            }
+        }
+    }
+}
+
+void adjacency_list_constructor_verbose(HPO molecules[],coordinates boxlength,int no_of_molecules,stack adjacency_list[])
+{
+    int i,j;
+    for(i=0;i<no_of_molecules;i++)
+    {
+        adjacency_list[i].top=NULL;
+        adjacency_list[i].length=0;
+    }
+    for(i=0;i<no_of_molecules-1;i++)
+    {
+        for(j=i+1;j<no_of_molecules;j++)
+        {
+            if(connected_molecules(&molecules[i],&molecules[j],boxlength))
+            {
+                add_node_given_value(&adjacency_list[i],j);
+                add_node_given_value(&adjacency_list[j],i);
 
                 //Shows which molecules are connected
                 printf("Nodes %d and %d are connected\n",i,j);
@@ -42,12 +63,14 @@ int main(int argc,char *argv[])
     int start_mol_no=-1;
     int no_of_molecules=0;
     int i;
+    int verbose_level=0;
+    int check_strict_flag=0;
     HPO molecules[MAX_M]; //only for 5000 molecules
     //static cluster_mt cluster[MAX_M];//for all the clusters
     coordinates boxlength;//, coordinate;
-    /*------------------------- read the arguments-------------------------*/
+    /*------------------------- START: read the arguments-------------------------*/
     int c;
-    while(( c = getopt(argc, argv, "f:o:")) != -1 )
+    while(( c = getopt(argc, argv, "f:o:v:c")) != -1 )
     {
         switch(c)
         {
@@ -65,50 +88,102 @@ int main(int argc,char *argv[])
                     exit(0); 
                 }
                 break;
+            case 'v':
+                verbose_level=atoi(optarg);
+                if((verbose_level<1)||(verbose_level>4))
+                {
+                    printf("Unknown Verbose level = %s\n",optarg);
+                    printf("Verbose level 1 -> Number of clusters and max size\n");
+                    printf("Verbose level 2 -> Statistics of connectedness and molecules\n");
+                    printf("Verbose level 3 -> Complete information of clusters and molecule neighbours\n");
+                    printf("Verbose level 4 -> Verbose level 3 and molecular postions of all molecules\n");
+                    exit(0);
+                }
+                break;
+            case 'c':
+                check_strict_flag=1;
+                break;
+            case '?':
+                if (optopt=='v')
+                {
+                    printf("Verbose level 1 -> Number of clusters and max size\n");
+                    printf("Verbose level 2 -> Statistics of connectedness and molecules\n");
+                    printf("Verbose level 3 -> Complete information of clusters and molecule neighbours\n");
+                    printf("Verbose level 4 -> Verbose level 3 and molecular postions of all molecules\n");
+                }
+                exit(0);
+
         }
     }
-    /*------------------------- read the arguments-------------------------*/
+    /*------------------------- END: read the arguments-------------------------*/
 
 
-    /*---------------------------- read the file --------------------------*/
+    /*-------------------------START: read the file --------------------------*/
 
-    //Populate the array after reading PDB
-    PDB_reader(fp_in,molecules,boxlength,&no_of_molecules,&start_mol_no);
-
-    //Prints all the molecules
-    // printf("%d %d",start_mol_no,end_mol_no);
-    // for(i=start_mol_no;i<end_mol_no;i++)
-    //     print_HPO(molecules[i]);
-
-    /*---------------------------- read the file --------------------------*/
+    PDB_reader_universal(fp_in,molecules,boxlength,&no_of_molecules,&start_mol_no);
 
     //Check if strictness matters
-    strict_vs_relaxed(molecules,boxlength,no_of_molecules);
+    if(check_strict_flag)
+    {
+        strict_vs_relaxed(molecules,boxlength,no_of_molecules);
+        printf("\n");
+    }
 
+    if(verbose_level>=1)
+        printf("Nummber of molecules: %d\n\n",no_of_molecules);
+    if (verbose_level>=3)
+        printf("BoxLength = %lf %lf %lf \n\n",boxlength[0],boxlength[1],boxlength[2]);
+    if (verbose_level>=4)
+    {
+        //Prints all the molecules
+        for(i=0;i<no_of_molecules;i++)
+            print_HPO(&molecules[i]);
+        printf("\n");
+    }
 
+    /*-------------------------END: read the file --------------------------*/
 
+    //Constructing the graph by the means of an adjacency list
     stack adjacency_list[no_of_molecules];
-   // int visited[no_of_molecules]={-1};
-    adjacency_list_constructor(molecules,boxlength,no_of_molecules,adjacency_list);
+    if(verbose_level>=3)
+    {
+        adjacency_list_constructor_verbose(molecules,boxlength,no_of_molecules,adjacency_list);
+        printf("\n");
+    }
+    else
+        adjacency_list_constructor(molecules,boxlength,no_of_molecules,adjacency_list);
 
-    /*------------------------connectedness calculations-----------------*/
+    /*------------------------START: connectedness calculations-----------------*/
     
     //Printing the adjacency list and connectedness
-    for(i=0;i<no_of_molecules;i++)
+    if(verbose_level>=3)
     {
-        printf("Molecule %d\t| Connected to %d molecules | Stack : ",i,adjacency_list[i].length);
-        print_stack(&adjacency_list[i]);
+        for(i=0;i<no_of_molecules;i++)
+        {
+            printf("Molecule %d\t| Connected to %d molecules | Stack : ",i,adjacency_list[i].length);
+            print_stack(&adjacency_list[i]);
+        }
+        printf("\n");
     }
+
 
     //Statistics of connectedness
     int connectedness[5];
     for(i=0;i<no_of_molecules;i++)
         connectedness[adjacency_list[i].length]++;
-    for(i=0;i<5;i++)
-        printf("The number of molecules with %d connections is %d\n",i,connectedness[i]);
 
-    /*------------------------connectedness calculations-----------------*/
+    if(verbose_level>=2)
+    {
+        for(i=0;i<5;i++)
+            printf("The number of molecules with %d connections is %d\n",i,connectedness[i]);
+        printf("\n");
+    }
 
+
+    /*-----------------------END: connectedness calculations-----------------*/
+
+    //The visited array is initialized to check if the molecules is visited during DFS.
+    //After running dfs it contains the cluster number of the molecules with its index
     int visited[no_of_molecules];
     for(i=0;i<no_of_molecules;i++)
         visited[i]=-1;
@@ -127,9 +202,13 @@ int main(int argc,char *argv[])
     }
     */
 
-    /*--------------------------clustering calculations------------------*/
+    /*-----------------------START: clustering calculations------------------*/
 
-    printf("Number of clusters: %d\n",number_of_clusters);
+    if(verbose_level>=1)
+    {
+        printf("Number of clusters: %d\n",number_of_clusters);
+        printf("\n");
+    }
 
 
     //Printing the cluster number
@@ -147,24 +226,37 @@ int main(int argc,char *argv[])
         cluster[i].length=0;
         cluster[i].top=NULL;
     }
+
     for(i=0;i<no_of_molecules;i++)
         add_node_given_value(&cluster[visited[i]],i);
+
+    
     for(i=0;i<number_of_clusters;i++)
     {
-        printf("Cluster %d\t| Cluster size : %d | Molecules: ",i,cluster[i].length);
-        print_stack(&cluster[i]);
-
         cluster_size[cluster[i].length]++;
         if(cluster[i].length>cluster_max_size)
             cluster_max_size=cluster[i].length;
     }
 
-    for(i=0;i<=cluster_max_size;i++)
+    if(verbose_level>=3)
+    {
+        for(i=0;i<number_of_clusters;i++)
+        {
+            printf("Cluster %d\t| Cluster size : %d | Molecules: ",i,cluster[i].length);
+            print_stack(&cluster[i]);
+        }
+        printf("\n");
+    }
+
+
+    if (verbose_level>=2)
+    {
+        for(i=1;i<=cluster_max_size;i++)
         printf("The number of clusters with %d molecules is %d\n",i,cluster_size[i]);
+        printf("\n");
+    }
 
-
-
-    /*--------------------------clustering calculations------------------*/
+    /*--------------------------END: clustering calculations------------------*/
 
 
     return 0;
